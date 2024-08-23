d29b23b392923a25909fe2d0d2937867bb09d636
sassanghz <sassanghazi@gmail.com>
2024-08-23 15:20:15 -0600

    Alert Header File:

--- include/alert.h
+++ include/alert.h
@@ -7,4 +7,10 @@
 #ifndef ALERT_H
 #define ALERT_H
 
+#include "contact.h"
+
+void trigger_alert(const char *message, Contact contacts[], int contact_count);
+void send_email(const char *recipient_email, const char *subject, const char *message_body);
+
+// Add a function for sending an SMS
 #endif //ALERT_H
