6e9563696d8314a690156e60bd30edde214250f7
sassanghz <sassanghazi@gmail.com>
2024-08-23 17:27:46 -0600

    Updated

--- src/alert.c
+++ src/alert.c
@@ -1,3 +1,8 @@
 //
 // Created by sassa on 2024-08-23.
 //
+#include <stdio.h>
+#include "alert.h"
+#include <curl/curl.h>
+
+
--- src/contact.c
+++ src/contact.c
@@ -7,7 +7,44 @@
 #include <string.h>
 #include "contact.h"
 
+void add_contact(Contact contacts[], int *contact_count) {
+    if(*contact_count >= 100) {
+        printf(("Contact list is full!\n"));
+        return;
+    }
 
+    Contact new_contact;
+    printf("Enter name: ");
+    scanf("%s", new_contact.name);
+    printf("Enter email: ");
+    scanf("%s", new_contact.email);
+    printf("Enter phone number: ");
+    scanf("%s", new_contact.phone);
+
+    contacts[*contact_count] = new_contact;
+    (*contact_count)++;
+    printf("Contact added successfully!\n");
+}
+
+void edit_contact(Contact contacts[], int contact_count) {
+
+}
+
+void remove_contact(Contact contacts[], int *contact_count) {
+    // Implementation as discussed earlier.
+}
+
+void display_contacts(Contact contacts[], int contact_count) {
+    // Implementation as discussed earlier.
+}
+
+void save_contacts_to_file(Contact contacts[], int contact_count, const char *filename) {
+    // Implementation as discussed earlier.
+}
+
+void load_contacts_from_file(Contact contacts[], int *contact_count, const char *filename) {
+    // Implementation as discussed earlier.
+}
 
 
 
